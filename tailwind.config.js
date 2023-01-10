c3faea0e9182bfa332fef7e8afaef9c69c1e76d2
justmirshod <98507846+justmirshod@users.noreply.github.com>
2023-01-10 21:56:07 +0500

    `initial config`

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: "#503E9D",
+        orange: "#FB6D3A",
+        green: "#6FD181",
+        dark: "#181B1A",
+        "purple-light": "#6553B2",
+      },
+    },
+  },
+  plugins: [],
+};
